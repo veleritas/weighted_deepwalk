b8950be680c924edf35477c7465ed17598645d16
Tong Shu Li <tongli@scripps.edu>
2018-10-19 12:00:55 -0700

    Fix indentation for Graph class
    Change indentation from 2 to 4 spaces.

--- deepwalk/graph.py
+++ deepwalk/graph.py
@@ -27,121 +27,117 @@ __email__ = "bperozzi@cs.stonybrook.edu"
 LOGFORMAT = "%(asctime).19s %(levelname)s %(filename)s: %(lineno)s %(message)s"
 
 class Graph(defaultdict):
-  """Efficient basic implementation of nx `Graph' â€“ Undirected graphs with self loops"""  
-  def __init__(self):
-    super(Graph, self).__init__(list)
+    """Efficient basic implementation of nx `Graph' â€“ Undirected graphs with self loops"""  
+    def __init__(self):
+        super(Graph, self).__init__(list)
 
-  def nodes(self):
-    return self.keys()
+    def nodes(self):
+        return self.keys()
 
-  def adjacency_iter(self):
-    return self.iteritems()
+    def adjacency_iter(self):
+        return self.iteritems()
 
-  def subgraph(self, nodes={}):
-    subgraph = Graph()
+    def subgraph(self, nodes={}):
+        subgraph = Graph()
 
-    for n in nodes:
-      if n in self:
-        subgraph[n] = [x for x in self[n] if x in nodes]
+        for n in nodes:
+            if n in self:
+                subgraph[n] = [x for x in self[n] if x in nodes]
 
-    return subgraph
+        return subgraph
 
-  def make_undirected(self):
-    t0 = time()
+    def make_undirected(self):
+        t0 = time()
 
-    for v in self.keys():
-      for other in self[v]:
-        if v != other:
-          self[other].append(v)
+        for v in self.keys():
+            for other in self[v]:
+                if v != other:
+                    self[other].append(v)
 
-    t1 = time()
-    logger.info('make_directed: added missing edges {}s'.format(t1-t0))
+        t1 = time()
+        logger.info('make_directed: added missing edges {}s'.format(t1-t0))
 
-    self.make_consistent()
-    return self
+        self.make_consistent()
+        return self
 
-  def make_consistent(self):
-    t0 = time()
-    for k in iterkeys(self):
-      self[k] = list(sorted(set(self[k])))
+    def make_consistent(self):
+        t0 = time()
+        for k in iterkeys(self):
+            self[k] = list(sorted(set(self[k])))
 
-    t1 = time()
-    logger.info('make_consistent: made consistent in {}s'.format(t1-t0))
+        t1 = time()
+        logger.info('make_consistent: made consistent in {}s'.format(t1-t0))
 
-    self.remove_self_loops()
+        self.remove_self_loops()
 
-    return self
+        return self
 
-  def remove_self_loops(self):
+    def remove_self_loops(self):
+        removed = 0
+        t0 = time()
 
-    removed = 0
-    t0 = time()
+        for x in self:
+            if x in self[x]:
+                self[x].remove(x)
+                removed += 1
 
-    for x in self:
-      if x in self[x]:
-        self[x].remove(x)
-        removed += 1
+        t1 = time()
 
-    t1 = time()
+        logger.info('remove_self_loops: removed {} loops in {}s'.format(removed, (t1-t0)))
+        return self
 
-    logger.info('remove_self_loops: removed {} loops in {}s'.format(removed, (t1-t0)))
-    return self
+    def check_self_loops(self):
+        for x in self:
+            for y in self[x]:
+                if x == y:
+                    return True
 
-  def check_self_loops(self):
-    for x in self:
-      for y in self[x]:
-        if x == y:
-          return True
+        return False
 
-    return False
+    def has_edge(self, v1, v2):
+        return (v2 in self[v1]) or (v1 in self[v2])
 
-  def has_edge(self, v1, v2):
-    if v2 in self[v1] or v1 in self[v2]:
-      return True
-    return False
-
-  def degree(self, nodes=None):
-    if isinstance(nodes, Iterable):
-      return {v:len(self[v]) for v in nodes}
-    else:
-      return len(self[nodes])
-
-  def order(self):
-    "Returns the number of nodes in the graph"
-    return len(self)
-
-  def number_of_edges(self):
-    "Returns the number of nodes in the graph"
-    return sum([self.degree(x) for x in self.keys()])/2
-
-  def number_of_nodes(self):
-    "Returns the number of nodes in the graph"
-    return order()
-
-  def random_walk(self, path_length, alpha=0, rand=random.Random(), start=None):
-    """ Returns a truncated random walk.
-
-        path_length: Length of the random walk.
-        alpha: probability of restarts.
-        start: the start node of the random walk.
-    """
-    G = self
-    if start:
-      path = [start]
-    else:
-      # Sampling is uniform w.r.t V, and not w.r.t E
-      path = [rand.choice(list(G.keys()))]
-
-    while len(path) < path_length:
-      cur = path[-1]
-      if len(G[cur]) > 0:
-        if rand.random() >= alpha:
-          path.append(rand.choice(G[cur]))
+    def degree(self, nodes=None):
+        if isinstance(nodes, Iterable):
+            return {v:len(self[v]) for v in nodes}
         else:
-          path.append(path[0])
-      else:
-        break
-    return [str(node) for node in path]
+            return len(self[nodes])
+
+    def number_of_edges(self):
+        """Returns the number of nodes in the graph"""
+        return sum([self.degree(x) for x in self.keys()])/2
+
+    def number_of_nodes(self):
+        """Returns the number of nodes in the graph"""
+        return len(self)
+
+    def random_walk(self, path_length, alpha=0, rand=random.Random(), start=None):
+        """ Returns a truncated random walk.
+
+            path_length: Length of the random walk.
+            alpha: probability of restarts.
+            start: the start node of the random walk.
+        """
+        G = self
+
+        if start:
+            path = [start]
+        else:
+            # Sampling is uniform w.r.t V, and not w.r.t E
+            path = [rand.choice(list(G.keys()))]
+
+        while len(path) < path_length:
+            cur = path[-1]
+            if len(G[cur]) > 0:
+                if rand.random() >= alpha:
+                    path.append(rand.choice(G[cur]))
+                else:
+                    path.append(path[0])
+            else:
+                break
+
+        return [str(node) for node in path]
+
 
 # TODO add build_walks in here
 
